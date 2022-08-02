db9933669517a736acb29226b07af9a34a395809
Harshit-Samar <84465149+Harshit-Samar@users.noreply.github.com>
2022-08-02 12:06:33 +0530

    added the label

--- lexer.py
+++ lexer.py
@@ -1,34 +1,44 @@
 from sly import Lexer
 
 class proofChecker(Lexer):
-    # Set of token names
-    tokens = {EOS, COMMA, NUM, VAR, COMMENT, EOL, ROUND_OPEN, ROUND_CLOSE, CURLY_OPEN, CURLY_CLOSE, SQUARE_OPEN, SQUARE_CLOSE, NOT, OR, AND, THEN, IFF, SCOPE, RULE}
+    
+    # Set of token names.   
+    tokens = {EOS, COMMA, NUM, VAR, COMMENT, EOL, ROUND_OPEN, ROUND_CLOSE, CURLY_OPEN, CURLY_CLOSE, SQUARE_OPEN, SQUARE_CLOSE, NOT, OR, AND, THEN, IFF, SCOPE, RULE,BY,USING,CANCEL,ASSN,HYPO, LABEL}
 
     # String containing ignored characters between tokens
     ignore = ' \t'
 
-    # Regex rules for tokens
+    # Regular expression rules for tokens
     EOS = r';'
     COMMA = r','
+    LABEL = r'^l\d+'
     NUM = r'[0-9]+'
+
     VAR = r'[a-zA-Z][a-zA-Z0-9_]*'
+    
     COMMENT = r'//.*'
     EOL = r'\n'
-    ROUND_OPEN = r'('
-    ROUND_CLOSE = r')'
-    CURLY_OPEN = r'{'
-    CURLY_CLOSE = r'}'
-    SQUARE_OPEN = r'['
-    SQUARE_CLOSE = r']'
+    ROUND_OPEN = r'\('
+    ROUND_CLOSE = r'\)'
+    CURLY_OPEN = r'\{'
+    CURLY_CLOSE = r'\}'
+    SQUARE_OPEN = r'\['
+    SQUARE_CLOSE = r'\]'
+    RULE = r'(~E|~I|&E|&I|\|E|\|I|>E|>I|<>E|<>I)'
     NOT = r'(\~|\!)'
     OR = r'\|'
     AND = r'\&'
     THEN = r'\>'
     IFF = r'\<\>'
     SCOPE = r'-'
-    RULE = r'(~E|~I|&E|&I|\|E|\|I|>E|>I|<>E|<>I)'
-    BY = r'by'
-    USING = r'using'
-    CANCEL = r'cancel'
-    ASSN = r'Assumption'
-    HYPO = r'Hypothesis'
+   
+     # Base ID rule
+    
+
+    # Special cases
+    VAR['by'] = BY
+    VAR['using'] = USING
+    VAR['cancel'] = CANCEL
+    VAR['Assumption'] = ASSN
+    VAR['Hypothesis'] = HYPO
+    # VAR['[a-n]'] = LABEL
