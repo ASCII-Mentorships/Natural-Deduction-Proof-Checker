cb62321e68827f276d7769ad6ada366d58fbf04f
Harshit-Samar <84465149+Harshit-Samar@users.noreply.github.com>
2022-08-02 12:10:20 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,48 +9,139 @@ class PCParser(Parser):
     tokens = proofChecker.tokens
     g_scp = 0
     t_scp = 0
-    stmt_no = 1
-    dict..
+   
+    dict1 = {}
+    @_('LABEL stmt')
+    def lab_stmt(self,p):
+        
+     
+        return [p.LABEL , p.stmt]
 
     @_('stmt_list stmt')
     def stmt_list(self,p):
-        evaluated_stmt = p.stmt
-        
-        return p
+        # print("1")        
+        return [p.stmt_list,p.stmt]
+
+   
 
     @_('stmt')
-    def stmt_list(self, p):
-        return p
+    def stmt_list(self,p):
+        # print("2") 
+        return p.stmt
 
-    @_('SCOPE stmt')
+   
+
+    @_(' SCOPE stmt')
     def stmt(self,p):
-        t_scp += 1
-        return p
-    
-    @_('expr SQUARE_OPEN reason SQUARE_CLOSE')
+        # print("3") 
+        # dict1[p.LABEL].append(p.stmt)       
+        return p.stmt
+
+    @_('expr SQUARE_OPEN reason SQUARE_CLOSE') # When it sees reason it finds where is def reason and then only checks @_
     def stmt(self,p):
-        if abs(self.t_scp-self.g_scp)>1:
-            print("\nMore than 1 scope changed in a single statement")
-            raise Exception("Scope Error")
-        
-        resolved_expr = p.expr
-        reason_list = p.reason
-        ["p", ">", "q"]
-        return []
-
-        evltd = p.expr
-        self.g_scp = self.t_scp
-        self.t_scp = 0
-        self.stmt_no += 1
-
+        # print("4")        
+        return [p.expr, p.reason]
+       
 
     @_('EOL')
     def stmt(self,p):
+        self.cnt += 1
         return p
-	
+
     @_('COMMENT')
     def stmt(self,p):
         return p
 
-    #########################
 
+    #############expr###############
+
+    @_('NOT expr')
+    def expr(self,p):
+        return['NOT' , p.expr]
+
+    @_('expr OR expr')
+    def expr(self,p):
+        return[ p.expr0, 'OR' , p.expr1]
+    
+    @_('expr AND expr')
+    def expr(self,p):
+        return[ p.expr0, 'AND' , p.expr1]
+
+    @_('expr THEN expr')
+    def expr(self,p):
+        return[ p.expr0, 'THEN' , p.expr1]
+    
+    @_('expr IFF expr')
+    def expr(self,p):
+        return[ p.expr0, 'IFF' , p.expr1]
+
+    @_('VAR')
+    def expr(self,p):
+        # print("5")
+        return  p.VAR
+    
+    @_('ROUND_OPEN expr ROUND_CLOSE')
+    def expr(self,p):
+        return p.expr 
+
+    ####################################
+
+
+    #################### Reason ##################
+
+    @_('reason_list')
+    def reason(self,p):
+        # print("6")
+        return p.reason_list
+
+    # @_('SQUARE_OPEN reason_list SQUARE_CLOSE ')
+    # def reason(self,p):
+    #     return p.reason_list
+
+
+    ################# reason_list #########
+
+    @_('ASSN')
+    def reason_list(self,p):
+        return p.ASSN
+
+
+    @_('HYPO')
+    def reason_list(self,p):
+        return  p.HYPO
+
+    @_(' BY RULE COMMA USING CURLY_OPEN expr_list CURLY_CLOSE')
+    def reason_list(self,p):
+        return [ p.RULE,p.expr_list]
+
+
+    @_('  BY RULE COMMA USING CURLY_OPEN expr_list CURLY_CLOSE COMMA CANCEL NUM')
+    def reason_list(self,p):
+        return [p.RULE,p.expr_list,p.NUM]
+
+    ###############expr_list###############
+
+    @_('expr_list COMMA NUM')
+    def expr_list(self,p):
+        return [p.expr_list, p.NUM]
+
+    
+    @_('NUM')
+    def expr_list(self,p):
+        return p.NUM
+
+if __name__ == '__main__':
+    lexer = proofChecker()
+    parser = PCParser()
+    env = {}
+    while True:
+        try:
+            text = input('basic > ')
+        except EOFError:
+            break
+        if text:
+           
+            for tok in lexer.tokenize(text):
+                print(tok)
+            tree = parser.parse(lexer.tokenize(text))
+            print(tree)
